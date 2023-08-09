8d9b62160f92a88b1414f519f2bdd8aa23ab24a2
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-09 00:28:24 -0300

    Change EF to Dapper

--- src/RinhaDeBackEnd.API/Models/Person.cs
+++ src/RinhaDeBackEnd.API/Models/Person.cs
@@ -4,19 +4,12 @@ namespace RinhaDeBackEnd.API.Models;
 
 public class Person
 {
-    [Key]
     public Guid Id { get; set; }
-
-    [Required] 
-    [StringLength(32, ErrorMessage = "O tamanho máximo é de {0} caracteres"), MinLength(2)]
+    
     public string Nickname { get; set; } = null!;
-
-    [Required] 
-    [StringLength(100, ErrorMessage = "O tamanho máximo é de {0} caracteres"), MinLength(2)]
+    
     public string Name { get; set; } = null!;
     
-    [Required]
-    [DisplayFormat(DataFormatString = "YYYY/MM/DD")]
     public DateOnly DateOfBirth  { get; set; }
     
     public string[]? Stack { get; set; } 
--- src/RinhaDeBackEnd.API/Program.cs
+++ src/RinhaDeBackEnd.API/Program.cs
@@ -1,9 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using RinhaDeBackEnd.API.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseInMemoryDatabase(databaseName: "RinhaDb"));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
