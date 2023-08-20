2622e322bcc4bee2cc742fe71ab45d12b8de44c6
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-20 18:41:55 -0300

    Refactor connection use on repository

--- src/RinhaDeBackEnd.API/Repositories/PessoaRepository.cs
+++ src/RinhaDeBackEnd.API/Repositories/PessoaRepository.cs
@@ -17,57 +17,72 @@ public class PessoaRepository : IPessoaRepository
     public async Task<Pessoa> Add(Pessoa pessoa)
     {
         pessoa.Id = Guid.NewGuid();
-        
-        await _dbConnection.ExecuteAsync("INSERT INTO pessoas VALUES (@Id, @Apelido, @Nome, @Nascimento, @Stack)",
-            new {
-            id = pessoa.Id,
-            apelido = pessoa.Apelido,
-            nome = pessoa.Nome,
-            nascimento = pessoa.Nascimento,
-            stack = pessoa.Stack
-            });
 
-        return pessoa;
+        using (_dbConnection)
+        {
+            await _dbConnection.ExecuteAsync("INSERT INTO pessoas VALUES (@Id, @Apelido, @Nome, @Nascimento, @Stack)",
+                new {
+                id = pessoa.Id,
+                apelido = pessoa.Apelido,
+                nome = pessoa.Nome,
+                nascimento = pessoa.Nascimento,
+                stack = pessoa.Stack
+                });
+            
+            return pessoa;
+        }
     }
     
     public IEnumerable<Pessoa> Get()
     {
-        var pessoas = _dbConnection.Query<Pessoa>(
-            "SELECT Id, Apelido, Nome, Nascimento, Stack FROM pessoas");
-        
-        return pessoas;
+        using (_dbConnection)
+        {
+            var pessoas = _dbConnection.Query<Pessoa>(
+                "SELECT Id, Apelido, Nome, Nascimento, Stack FROM pessoas");
+            
+            return pessoas;
+        }
     }
 
     public async Task<Pessoa> GetById(Guid id)
     {
-        var pessoa = await _dbConnection.QuerySingleOrDefaultAsync<Pessoa>(
-            @"SELECT Id, Apelido, Nome, Nascimento, Stack FROM pessoas WHERE Id = @Id", 
-            new { Id = id });
-        
-        return pessoa;
+        using (_dbConnection)
+        {
+            var pessoa = await _dbConnection.QuerySingleOrDefaultAsync<Pessoa>(
+                @"SELECT Id, Apelido, Nome, Nascimento, Stack FROM pessoas WHERE Id = @Id", 
+                new { Id = id });
+            
+            return pessoa;
+        }
     }
     
     public async Task<Pessoa> GetByApelido(string apelido)
     {
-        var pessoa = await _dbConnection.QuerySingleOrDefaultAsync<Pessoa>(
-            @"SELECT Id, Apelido, Nome, Nascimento, Stack FROM pessoas WHERE Apelido = @Apelido", 
-            new { Apelido = apelido });
-        
-        return pessoa;
+        using (_dbConnection)
+        {
+            var pessoa = await _dbConnection.QuerySingleOrDefaultAsync<Pessoa>(
+                @"SELECT Id, Apelido, Nome, Nascimento, Stack FROM pessoas WHERE Apelido = @Apelido", 
+                new { Apelido = apelido });
+            
+            return pessoa;
+        }
     }
     
     public async Task<IEnumerable<Pessoa>> FindByTerm(string term)
     {
-        var pessoas = await _dbConnection.QueryAsync<Pessoa>(
-            @"SELECT *
-                FROM pessoas
-                WHERE Nome ILIKE '%' || @Term || '%'
-	                OR Apelido ILIKE '%' || @Term || '%'
-	                OR @Term ILIKE SOME(Stack)
-                LIMIT 50;", 
-            new { Term = term } );
-
-        return pessoas;
+        using (_dbConnection)
+        {
+            var pessoas = await _dbConnection.QueryAsync<Pessoa>(
+                @"SELECT *
+                    FROM pessoas
+                    WHERE Nome ILIKE '%' || @Term || '%'
+	                    OR Apelido ILIKE '%' || @Term || '%'
+	                    OR @Term ILIKE SOME(Stack)
+                    LIMIT 50;", 
+                new { Term = term } );
+            
+            return pessoas;
+        }
     }
 
 
@@ -83,8 +98,11 @@ public class PessoaRepository : IPessoaRepository
 
     public async Task<int> Count()
     {
-        var count = await _dbConnection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM pessoas");
-
-        return count;
+        using (_dbConnection)
+        {
+            var count = await _dbConnection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM pessoas");
+            
+            return count;
+        }
     }
 }
